1bcb85d345c395cecd837bdb97ce0b78e55edb2d
Yusuf Pazuruan <yusufpazuruan@gmail.com>
2023-07-13 12:19:25 +0700

    add notes

--- src/notes.js
+++ src/notes.js
@@ -1,3 +1,68 @@
-const notes = [];
+// const salesData = [
+//   {
+//     id: 1,
+//     productName: 'Product A',
+//     price: 10.99,
+//     quantity: 5,
+//     customer: 'Customer 1',
+//     date: '2023-07-01'
+//   },
+//   {
+//     id: 2,
+//     productName: 'Product B',
+//     price: 5.99,
+//     quantity: 10,
+//     customer: 'Customer 2',
+//     date: '2023-07-02'
+//   },
+//   {
+//     id: 3,
+//     productName: 'Product C',
+//     price: 8.99,
+//     quantity: 3,
+//     customer: 'Customer 1',
+//     date: '2023-07-03'
+//   },
+//   {
+//     id: 4,
+//     productName: 'Product A',
+//     price: 10.99,
+//     quantity: 2,
+//     customer: 'Customer 3',
+//     date: '2023-07-04'
+//   }
+// ];
+
+const notes = [
+  {
+    id: 1,
+    title: "Meeting Notes",
+    content: "Discussed project timeline and deliverables.",
+    createdBy: "John",
+    createdAt: "2023-07-01T09:00:00Z",
+  },
+  {
+    id: 2,
+    title: "Ideas for Marketing Campaign",
+    content: "Brainstormed ideas for upcoming marketing campaign.",
+    createdBy: "Jane",
+    createdAt: "2023-07-02T14:30:00Z",
+  },
+  {
+    id: 3,
+    title: "Action Items",
+    content: "Outlined action items from the team meeting.",
+    createdBy: "Mark",
+    createdAt: "2023-07-03T11:15:00Z",
+  },
+  {
+    id: 4,
+    title: "Project Update",
+    content: "Provided an update on the project status.",
+    createdBy: "Sarah",
+    createdAt: "2023-07-04T16:45:00Z",
+  },
+];
+
  
 module.exports = notes;
